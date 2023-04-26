8ea00d4ea061c839468134292000601c947ae943
Ross Newman <rzn3102@gmail.com>
2023-04-26 18:09:59 -0400

    set up babel and express

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import express from 'express';
+import routes from './routes';
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+// Middleware
+app.use(cors());
+
+// Routes
+app.use('/tasks', routes.tasks);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () =>
+  console.log(`Example app is listening on port ${PORT}`)
+);
+
+export default app;
